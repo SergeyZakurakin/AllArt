78b9e78862f7f6fa6077c51e146bebed77ef9d89
SergeyZakurakin <pricezak@gmail.com>
2024-12-22 17:21:10 -0500

    Initial Commit

---
+++ AllArt/AllArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllArtApp.swift
+//  AllArt
+//
+//  Created by Sergey Zakurakin on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct AllArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
